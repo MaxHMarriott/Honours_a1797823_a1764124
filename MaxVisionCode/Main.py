b3b811c4598841a3705162bcb40689c036f53b25
MaxHMarriott <max@marriott.cc>
2023-04-14 13:45:48 +0930

    added findport function and savePhoto in Main.py

--- MaxVisionCode/Main.py
+++ MaxVisionCode/Main.py
@@ -3,8 +3,36 @@
 
 import cv2
 
+def savePhoto(frame,location,framecount):
+	cv2.imwrite(location,frame)
+	print("Photo of frame:") 
+	print(framecount)
+	print("saved at: " + location)
+	return
+
+
+def visionProcessing(frame):
+    #vision code goes here
+	return frame
+
+def findPort(port):
+	for a in range(9):
+		try: 
+			test = cv2.VideoCapture(port,cv2.CAP_V4L2)
+			print("Camera feed found at port:")
+			print(a)
+			port = a
+			del test
+			return port
+		except:
+			a = a
+	raise exception("No valid port found")
+	return port
+
 #Add function to find port
 port = 0
+findPort(port)
+framecount = 0
 print("Connecting to camera")
 cap = cv2.VideoCapture(port,cv2.CAP_V4L2) #Find port
 #cap = cv2.VideoCapture(1,cv2.CAP_FFMPEG) #Find port
@@ -23,18 +51,16 @@ while True:
     ret, frame = cap.read(cv2.CAP_V4L2)
     print("This has run 2")
     frame = cv2.resize(frame, None, fx=0.5, fy=0.5, interpolation=cv2.INTER_AREA)
+    framecount = framecount + 1
     #opencv code goes here
     #frame = visionProcessing(frame)
     cv2.imshow('Input', frame)
     print("Frame Displayed")
-
     c = cv2.waitKey(1)
     if c == 27: #escape key will close imshow window
-        break
-
+    	print("Number of frames taken are:")
+    	print(framecount)
+    	savePhoto(frame,"/home/teamvision/Desktop/Screenshots/capture.jpeg",framecount)
+    	break
 cap.release()
 cv2.destroyAllWindows()
-
-def visionProcessing(frame):
-    #vision code goes here
-    return frame
