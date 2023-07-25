3b66080cc54b3b3cd3473626f0ed67d8b7e19f1a
MaxHMarriott <max@marriott.cc>
2023-07-25 17:26:00 +0930

    added UGV1 Main and expanded custom msgs to include a custom message for detecting LED severity

---
+++ ROS_Workspace/src/UGV1Main/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'UGV1Main'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='max',
+    maintainer_email='max@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'UGV1Main = UGV1Main.UGV1Main:main',
+        ],
+    },
+)
